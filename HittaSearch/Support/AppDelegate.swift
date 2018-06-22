35c99392cbfddfc820539865f54e5e681362edef
doc <doc@docs-MBP.lan>
2018-06-22 22:30:18 +0100

    Added Textture pod

--- HittaSearch/Support/AppDelegate.swift
+++ HittaSearch/Support/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AsyncDisplayKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
---
+++ Podfile
@@ -0,0 +1,7 @@
+target 'HittaSearch' do
+    
+    use_frameworks!
+    
+    pod 'Texture', '>= 2.0'
+    
+end
